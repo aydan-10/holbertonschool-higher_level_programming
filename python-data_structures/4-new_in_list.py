7f423e9fdb538c3e53661ed937e63914d6f6f999
aydan-10 <aydanmahmudova2006@gmail.com>
2026-05-10 07:36:50 +0000

    Fix: remove whitespace from blank lines in Task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     # Orijinal siyahının nüsxəsini (copy) yaradırıq
     copy_list = my_list.copy()
-    
+
     if idx >= 0 and idx < len(my_list):
         copy_list[idx] = element
-        
+
     return copy_list
