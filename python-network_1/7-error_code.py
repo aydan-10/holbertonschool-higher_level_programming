5ea3fd07abbbccfe8f49ece38b04262fd76efc98
aydan-10 <aydanmahmudova2006@gmail.com>
2026-05-22 17:11:16 +0000

    Add task 7: Handle HTTP error status codes using requests

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""requests ilə URL sorğusu göndərən və HTTP xətalarını yoxlayan skript."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
