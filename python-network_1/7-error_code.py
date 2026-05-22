af0f4b9dcad506deb5e8139d882b450f0ae49be8
aydan-10 <aydanmahmudova2006@gmail.com>
2026-05-22 17:13:12 +0000

    Fix: Remove trailing whitespace on line 10 for pycodestyle

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -7,7 +7,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
