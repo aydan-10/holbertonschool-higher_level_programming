48bf41f90b46a50847d2de598ce8c2103c5b200f
aydan-10 <aydanmahmudova2006@gmail.com>
2026-05-22 17:00:00 +0000

    Add task 2: Send POST request with email parameter

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""URL ünvanına email parametri ilə POST sorğusu göndərən skript."""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    # Göndəriləcək datanı URL-encoded formata salırıq və baytlara çeviririk
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    
+    # POST sorğusunu icra edirik
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
