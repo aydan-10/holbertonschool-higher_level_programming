7bfdca7b17c915db5aff6ab4571e996a585e3387
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-03 16:40:29 +0000

    Update: Added docstrings and README.md

---
+++ 0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
+
+
+class Rectangle:
+    """An empty class Rectangle that defines a rectangle."""
+    pass
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Python - More Classes and Objects
+Rectangle class definition
