c5c5caa1b3294fbaa7a1c39d1d486b6cd4be7d9e
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-03 16:34:29 +0000

    Fix code formatting

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -8,3 +8,13 @@ Defines an empty class Rectangle.
 class Rectangle:
     """An empty class Rectangle that defines a rectangle."""
     pass
+#!/usr/bin/python3
+"""
+0-rectangle module
+Defines an empty class Rectangle.
+"""
+
+
+class Rectangle:
+    """An empty class Rectangle that defines a rectangle."""
+    pass
