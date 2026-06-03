1b3552822d51c9c65403140912ec9b0f5a54536c
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-03 16:36:42 +0000

    Add Task 0: Simple rectangle and README.md

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -8,13 +8,3 @@ Defines an empty class Rectangle.
 class Rectangle:
     """An empty class Rectangle that defines a rectangle."""
     pass
-#!/usr/bin/python3
-"""
-0-rectangle module
-Defines an empty class Rectangle.
-"""
-
-
-class Rectangle:
-    """An empty class Rectangle that defines a rectangle."""
-    pass
