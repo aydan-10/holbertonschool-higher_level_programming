ef75a3f345387e2d02486fddac323acadc4b03fb
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-05 15:54:03 +0000

    Fix PEP8 line length issue

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,12 +3,14 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is an instance or inherited instance of a class.
+    """Checks if an object is an instance or inherited instance.
 
     Args:
         obj: The object to check.
         a_class: The class to match the type of obj to.
+
     Returns:
-        True if obj is an instance or inherited instance of a_class, otherwise False.
+        True if obj is an instance or inherited instance,
+        otherwise False.
     """
     return isinstance(obj, a_class)
