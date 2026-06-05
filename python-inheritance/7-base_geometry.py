08da42376e63dda6aaf7cb73ff63fa291e4c8e4a
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-05 16:46:17 +0000

    Fix integer_validator to reject bool type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class BaseGeometry."""
+"""Module for BaseGeometry class."""
 
 
 class BaseGeometry:
-    """A class with public instance methods area and integer_validator."""
+    """A class with geometry tools."""
 
     def area(self):
-        """Raises an Exception as it is not implemented."""
+        """Raises an exception."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
+        """Validates value."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
