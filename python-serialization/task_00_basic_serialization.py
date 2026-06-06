adf99492654a3936276d120c7b642498f2945911
aydan-10 <aydanmahmudova2006@gmail.com>
2026-06-06 13:49:58 +0000

    Add basic serialization module

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for basic serialization."""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize data to a JSON file."""
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Load and deserialize data from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
